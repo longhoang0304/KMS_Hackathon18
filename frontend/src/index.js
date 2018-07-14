481122c79ad3b7a4fa8e06bdf50f0483fe26591a
3K Or Null <duynse@gmail.com>
2018-07-14 12:04:26 +0700

    Init basic sturcture

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import NavBar from './component/List';
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
